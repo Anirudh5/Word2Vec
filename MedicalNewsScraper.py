db7f50a2bfee9c204dea46584d3ec4dd8ba2c2d6
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2019-04-05 22:00:04 +0530

    Modificications: 1. Sequential scraping -> Multithreaded scraping 2. Handled 410 error bad requests 3. Extracting complete article text

--- MedicalNewsScraper.py
+++ MedicalNewsScraper.py
@@ -1,34 +1,37 @@
-from urllib import request
+import re
+import requests
+import threading
 from bs4 import BeautifulSoup
 
-def get_page_content(url):
-    page = request.urlopen(url).read().decode("utf-8", "ignore")
-    soup = BeautifulSoup(page, "html.parser")
-    article_body = soup.find("div", itemprop="articleBody")
-    try:
-        content = article_body.find("header").text
-    except:
-        print(url)
-        content = ""
-    paragraphs = article_body.find_all("p")
-    for p in paragraphs: content += p.text
-    content = content.strip()
-    outfile.write(content)
+class Scraper(threading.Thread):
+    def __init__(self, url, file):
+        threading.Thread.__init__(self)
+        self.page_url = url
+        self.file = open(str(i) + ".txt", "w")
 
-def get_article_links(page_url):
-    page = request.urlopen(page_url).read().decode("utf-8", "ignore")
-    soup = BeautifulSoup(page, "html.parser")
-    listing = soup.find("ul", class_="listing")
-    articles = listing.find_all("a")
-    for article in articles:
-        url = "https://www.medicalnewstoday.com" + article.get('href')
-        get_page_content(url)
+    def run(self):
+        page = requests.get(self.page_url, {'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(page, "html.parser")
+        listing = soup.find("ul", class_="listing")
+        articles = listing.find_all("a")
+        for article in articles:
+            url = "https://www.medicalnewstoday.com" + article.get('href')
+            try: self.get_page_content(url)
+            except Exception as e: print(url)
+
+    def get_page_content(self, url):
+        page = requests.get(url, {'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(page, "html.parser")
+        article_body = soup.find("div", itemprop="articleBody")
+        content = article_body.text
+        content = re.sub("Table of contents", "", content)
+        content = " ".join(content.split())
+        content = re.sub("googleAdSlotInfo(.*);", "", content)
+        self.file.write(content + "\n\n")
 
 if __name__ == '__main__':
-    outfile = open("medical_news.txt", "w")
     base_url = "https://www.medicalnewstoday.com/archive/"
     for i in range(45, -1, -1):
         if i == 0: page_url = base_url
         else: page_url = base_url + str(i)
-        get_article_links(page_url)
-        print("Page", i, "done")
+        Scraper(page_url, i).start()
