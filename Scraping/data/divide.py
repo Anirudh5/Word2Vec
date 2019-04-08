6c469d8d819212bb4f3cf7860d33770a96108881
Anirudh5 <=>
2019-04-08 14:51:55 +0530

    CHUNKED DATA FILES ADDED

---
+++ Scraping/data/divide.py
@@ -0,0 +1,16 @@
+import numpy as np
+f = open("naturedata.txt")
+lines = []
+for line in f:
+	line = line.strip()
+	lines.append(line)
+l = [i for i in range(len(lines))]
+l = np.array_split(l,10)
+for i in range(1,11):
+	data = ""
+	for id in l[i-1]:
+		data = data + lines[id] + "\n"
+	f1 = open("naturedata"+str(i)+".txt",'w')
+	f1.write(data)
+	f1.close()
+	print i
